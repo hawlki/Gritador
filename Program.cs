bb0ccc80f63f34816db35451d5bb489bfc6d1bef
israel lopes <israel.lopes2710@gmail.com>
2021-03-09 18:50:04 -0300

    muda titulo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Gritador
+# gritador
 Faça um programa repita exatamente que o usuário digitar, gritando (em CAIXA ALTA).
