58745592e9bc901d25f06870dcf112482ace16e1
israel lopes <israel.lopes2710@gmail.com>
2021-03-10 16:59:55 -0300

    grita

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace Gritador
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Console.Beep();
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Etec Adolpho Berezin");
+            Console.WriteLine("Feito por Israel Camilo Lopes");
+            Console.WriteLine("1I3");
+            Console.WriteLine("-----------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Por favor, digite alguma coisa:");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            String algumaCoisa = Console.ReadLine();
+            Console.ResetColor();
+            Console.WriteLine("-----------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine($"{algumaCoisa.ToUpper()}");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-----------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("para de gritar comigo!, vc pensa que eu sou só uma maquína?, eu sinto emoções também!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------------------------------------------------------------------------------------");
+            Console.ResetColor();
+            Console.WriteLine("sabe o que é teclado? Então...Aperta qualquer botão ai que vai limpar a sua tela :)");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
